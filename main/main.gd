84da91d204fc0cdfd6a0cde59694a12dda265f6a
Tarnest <tristanconnolly1@gmail.com>
2024-03-10 17:20:35 -0400

    Added Pellets, Added Coin Detector on Player, Implemented Pellets in Main

--- main/main.gd
+++ main/main.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
+@onready var pellets := $Pellets.get_children()
+
+func _ready() -> void:
+	for pellet in pellets:
+		pellet.connect("pellet_eaten", on_pellet_eaten)
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("escape"):
 		get_tree().quit()
+
+func on_pellet_eaten() -> void:
+	print("eaten")
